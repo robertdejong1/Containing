69dd0772f1d78c0a3737857bc35625cc1f24f33a
Yolo Swaggins <speedy@speedydown.nl>
2013-11-21 11:25:47 +0100

    We hebben een interface!

--- Containing/src/containing/UserInterface.java
+++ Containing/src/containing/UserInterface.java
@@ -1,15 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package containing;
 
-/**
- *
- * @author Robert
- */
-public class UserInterface {
-    
+import javax.swing.JFrame;
+
+
+
+public class UserInterface extends JFrame
+{
+    public UserInterface()
+    {
+        super();
+        this.setLayout(null);
+        this.setBounds(25, 25, 500, 500);
+        this.setVisible(true);
+    }
 }

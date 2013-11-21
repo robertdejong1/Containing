9205777d08781ffbd695b1a1b3438830efb9a9aa
Yolo Swaggins <speedy@speedydown.nl>
2013-11-21 10:40:10 +0100

    Clock class aangemaakt

--- Containing/src/containing/Clock.java
+++ Containing/src/containing/Clock.java
@@ -1,15 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package containing;
 
-/**
- *
- * @author Robert
- */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
 public class Clock {
+
+    private Timer timer;
+
+    public Clock() 
+    {
+        timer = new Timer(100, new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                TimerFunctions();
+            }
+        });
+
+        timer.setRepeats(true);
+    }
     
+    public boolean StartClock(int delay)
+    {
+        try
+        {
+            timer.setDelay(delay);
+            timer.start();
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+    
+    public boolean SetDelay(int delay)
+    {
+        try
+        {
+            timer.setDelay(delay);
+            timer.stop();
+            timer.start();
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }   
+    }
+    
+    public boolean StopClock()
+    {
+        try
+        {
+            timer.stop();
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+    
+    private void TimerFunctions()
+    {
+        //Controller doet hier iets :D
+    }
 }

24434d4d5dd69ffc5bca42fbe0359b07165271ae
Minardus van der Kooi <minardus@gmail.com>
2013-11-21 11:42:44 +0100

    fixed the merge fuckup

--- Containing/src/containing/Platform/StoragePlatform.java
+++ Containing/src/containing/Platform/StoragePlatform.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package containing.Platform;
 
 /**
@@ -19,20 +18,3 @@ public class StoragePlatform extends Platform {
     }
 
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package containing.Platform;
-
-/**
- *
- * @author Robert
- */
-public class StoragePlatform {
-    
-}
->>>>>>> 69dd0772f1d78c0a3737857bc35625cc1f24f33a
--- Containing/src/containing/Platform/TrainPlatform.java
+++ Containing/src/containing/Platform/TrainPlatform.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package containing.Platform;
 
 public class TrainPlatform extends Platform {
@@ -8,20 +7,3 @@ public class TrainPlatform extends Platform {
     }
 
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package containing.Platform;
-
-/**
- *
- * @author Robert
- */
-public class TrainPlatform {
-    
-}
->>>>>>> 69dd0772f1d78c0a3737857bc35625cc1f24f33a

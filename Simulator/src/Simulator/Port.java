971fc44dbeffc04f26b096a34eeca437016475e6
BenjaminFeenstra <benjaminfeenstra@hotmail.com>
2013-11-25 11:24:01 +0100

    .

---
+++ Simulator/src/Simulator/Port.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Simulator;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.scene.Node;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class Port extends Model
+{
+    
+    public Port(AssetManager assetManager, Node node)
+    {
+        super(assetManager, "Models/port.j3o", node);
+    }
+    
+}

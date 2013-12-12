0f80cc6e1bc50b2cdf02fde065c60cb5df8f2e54
Yolo Swaggins <speedy@speedydown.nl>
2013-12-12 14:20:40 +0100

    road in port

--- Containing/src/containing/Port.java
+++ Containing/src/containing/Port.java
@@ -6,7 +6,7 @@ import containing.Platform.SeashipPlatform;
 import containing.Platform.StoragePlatform;
 import containing.Platform.TrainPlatform;
 import containing.Platform.TruckPlatform;
-import containing.Vehicle.AGV;
+import containing.Road.Road;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +15,11 @@ public class Port implements Serializable
 {
     private int ID = 0;
     private List<Platform> Platforms;
+    private Road Mainroad;
+
+    public Road getMainroad() {
+        return Mainroad;
+    }
     //private List<AGV> aGVs;
     private StoragePlatform storagePlatform;
     
@@ -28,6 +33,13 @@ public class Port implements Serializable
         Platforms.add(new SeashipPlatform(new Vector3f(0, 0, 0)));
         Platforms.add(new TrainPlatform(new Vector3f(0, 5.5f, 0)));
         Platforms.add(new TruckPlatform(new Vector3f(0, 0, 0)));
+        
+        List<Vector3f> waypointList = new ArrayList<>();
+        waypointList.add(new Vector3f(103,5.5f,0));
+        waypointList.add(new Vector3f(103,5.5f, 1562));
+        waypointList.add(new Vector3f(717,5.5f, 1562));
+        waypointList.add(new Vector3f(717, 5.5f, 0));
+        Mainroad = new Road(waypointList);
         
         storagePlatform = new StoragePlatform(new Vector3f(110f*Settings.METER , 5.5f, 19f*Settings.METER));
         //aGVs = storagePlatform.getAllCreatedAgvs();

9e37f22f5a2d15aee67dd972a8cf437eed503d64
robertdj20 <robertdj20@gmail.com>
2013-12-13 14:39:50 +0100

    App werkt weer

--- App/src/containing/app/CommandHandler.java
+++ App/src/containing/app/CommandHandler.java
@@ -1,58 +1,67 @@
 package containing.app;
 
+import android.util.Base64;
+import containing.Command;
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 
 /**
  *
  * @author Robert
  */
+enum Type {
+
+    TRUCK, AGV, BARGE, BARGECRANE, SEASHIP, SEASHIPCRANE, TRAIN, TRAINCRANE, TRUCKCRANE, STORAGECRANE
+}
+
 public class CommandHandler {
 
-	volatile static List<String> queuedCommands = new ArrayList<String>();
-	
-    public static void handle(String input){
-        JSONObject json = (JSONObject) JSONValue.parse(input);
+    private static volatile List<String> queuedCommands = new ArrayList<String>();
 
-        if(json.get("stats") != null){
-        	
-        	JSONObject stats = (JSONObject) json.get("stats");
-
-        	HashMap<String, Double> map = new HashMap<String, Double>();
-        	map.put("train", Double.parseDouble(stats.get("train").toString()));
-        	map.put("truck", Double.parseDouble(stats.get("truck").toString()));
-        	map.put("seaShip", Double.parseDouble(stats.get("seaShip").toString()));
-        	map.put("barge", Double.parseDouble(stats.get("barge").toString()));
-        	map.put("storage", Double.parseDouble(stats.get("storage").toString()));
-        	map.put("agv", Double.parseDouble(stats.get("agv").toString()));
-        	map.put("other", Double.parseDouble(stats.get("other").toString()));
-        	
+    static void handle(String input) {
+        //Handle de command
+        Command cmd = (Command) decode(input);
+        
+        if(cmd.getCommand().equals("stats")){
         	if(MainActivity.getStats() == null){
-        		MainActivity.showGraphView(map);
+        		MainActivity.showGraphView((HashMap<String, Double>) cmd.getObject());
         	}
         	else{
-        		MainActivity.updateStats(map);
+        		MainActivity.updateStats((HashMap<String, Double>) cmd.getObject());
         	}
         }
     }
-    
-    public static List<String> getCommands(){
+
+    private static Object decode(String encoded) {
+        byte[] bytes;
+        bytes = Base64.decode(encoded, Base64.DEFAULT);
+
+        try {
+            ByteArrayInputStream bi = new ByteArrayInputStream(bytes);
+            ObjectInputStream si = new ObjectInputStream(bi);
+            return si.readObject();
+        } catch (Exception e) {
+            System.out.println("An error occured");
+        }
+        return null;
+    }
+
+    public static List<String> getCommands() {
         return queuedCommands;
     }
-    
-    public static void clearCommands(){
-    	queuedCommands.clear();
+
+    public static void clearCommands() {
+        queuedCommands.clear();
     }
-    
-    public static void addCommand(String cmd){
-    	queuedCommands.add(cmd);
+
+    public static void addCommand(String cmd) {
+        queuedCommands.add(cmd);
     }
-    
-    public static boolean newCommandsAvailable(){
-    	return queuedCommands.size() > 0;
+
+    public static boolean newCommandsAvailable() {
+        return queuedCommands.size() > 0;
     }
 }

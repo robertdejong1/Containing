3ed4fb33d625730d7690d00943cd198ee2b0b41e
Gemirt <mairimoz@hotmail.com>
2013-12-16 14:47:36 +0100

    originated

--- Containing/src/containing/Road/Road.java
+++ Containing/src/containing/Road/Road.java
@@ -30,16 +30,15 @@ public class Road implements Serializable
     
 
     private Vector3f createCorrespondingWaypoint(Vector3f point){
-        return point;
-        /*
+        
+        
         if (track.size() == 4){
             if (point.x < track.get(0).x){ return new Vector3f(track.get(0).x, point.y, point.z);}
-            if (point.y > track.get(2).x) {return new Vector3f(track.get(2).x, point.y, point.z); }
+            if (point.x > track.get(2).x) {return new Vector3f(track.get(2).x, point.y, point.z); }
             if (point.z < track.get(1).z){return new Vector3f(point.x, point.y, track.get(1).z);}
             return new Vector3f(point.x,point.y, track.get(0).z);
         }
         return new Vector3f(track.get(0).x, point.y, point.z);
-        * */
     }
     
    

9192d483b5890fdfe841bae2cf3a4198097679fb
Gemirt <mairimoz@hotmail.com>
2013-12-16 10:39:35 +0100

    road

--- Containing/src/containing/Road/Road.java
+++ Containing/src/containing/Road/Road.java
@@ -2,6 +2,7 @@ package containing.Road;
 
 import containing.ParkingSpot.ParkingSpot;
 import containing.Platform.Platform;
+import containing.Platform.StoragePlatform;
 import containing.Settings;
 import containing.Vector3f;
 import containing.Vehicle.AGV;
@@ -9,29 +10,12 @@ import containing.Vehicle.ExternVehicle;
 import containing.Vehicle.Vehicle;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Road implements Serializable 
 {
     float roadWidth = AGV.width + AGV.width * 0.25f;
-    //testvariabelen, simulatie is bepalend 
-    Vector3f vertex_outsidetrack_leftbottom = new Vector3f(0,0,0); 
-    Vector3f vertex_outsidetrack_rightbottom = new Vector3f(0,0,100);
-    Vector3f vertex_outsidetrack_lefttop = new Vector3f(100,0,0);
-    Vector3f vertex_outsidetrack_righttop = new Vector3f(100,0,100);
-    
-    Vector3f vertex_insidetrack_leftbottom = new Vector3f(vertex_outsidetrack_leftbottom.x + roadWidth,0,vertex_outsidetrack_leftbottom.z + roadWidth); 
-    Vector3f vertex_insidetrack_rightbottom = new Vector3f(vertex_outsidetrack_rightbottom.x - roadWidth,0,vertex_outsidetrack_rightbottom.z + roadWidth);
-    Vector3f vertex_insidetrack_lefttop = new Vector3f(vertex_outsidetrack_lefttop.x + roadWidth,0,vertex_outsidetrack_lefttop.z - roadWidth);
-    Vector3f vertex_insidetrack_righttop = new Vector3f(vertex_outsidetrack_righttop.x - roadWidth,0,vertex_outsidetrack_righttop.z - roadWidth);
-    
-    //List<Vector3f> outsidetrack = new ArrayList<>();
-    
-    //List<Vector3f> insidetrack = new ArrayList<>();
-    
-    //List<Vector3f> singletrack = new ArrayList<>();
     
     List<Vector3f> track = new ArrayList<>();
         
@@ -92,15 +76,15 @@ public class Road implements Serializable
          track.add(this.createCorrespondingWaypoint(sourcePlatformExitPoint));
          track.add(this.createCorrespondingWaypoint(destination.getEntrypoint()));
          track.add(destination.getEntrypoint());
-         Collections.sort(track);
-         track = this.setPathCorrectOrder(track, sourcePlatformExitPoint, destination.getExitpoint());
+       
+         track = this.setPathCorrectOrder(track, vehicle.getCurrentPlatform(), destination);
          vehicle.setCurrentPlatform(destination);
          vehicle.setPosition(destination.getEntrypoint());
          return new Route(track, getPathLength(track));
          
      }
      
-     //v an ingang platform naar parkeerplaats
+     //van ingang platform naar parkeerplaats
       public Route getPath(Vehicle vehicle, ParkingSpot ps)
       {
           List<Vector3f> track = this.track;
@@ -138,53 +122,7 @@ public class Road implements Serializable
     }
      
     
-     /*
     
-    //public Route getPath(Vehicle vehicle, VehicleSpot)
-    public Route getPath(Vehicle vehicle, Platform destination, boolean outin){ 
-        //\\
-        
-        
-        Route shortestPath = null;
-        if (outin)//ingang
-        {
-            Vector3f entrypoint = this.createCorrespondingWaypoint(destination.getEntrypoint());
-            shortestPath = calculateShortestPath(vehicle, entrypoint);
-      
-        }
-        else
-        {
-            Vector3f exitpoint = this.createCorrespondingWaypoint(destination.getExitpoint());
-            shortestPath = calculateShortestPath(vehicle.getPosition(), exitpoint);
-        }
-        
-        shortestPath.setDestinationPlatform(destination);
-        shortestPath.setDestinationParkingSpot(null);
-        
-        
-        return shortestPath; 
-    }
-    
-    //70 130 152
-    
-  
-    
-    public Route getPath(Vehicle vehicle, ParkingSpot ps){
-        Vector3f source = this.createCorrespondingWaypoint(vehicle.getPosition());
-        Vector3f destination = this.createCorrespondingWaypoint(ps.getPosition());
-        List<Vector3f> track = new ArrayList<Vector3f>(this.track);
-        track.add(source);
-        track.add(destination);
-        Collections.sort(track);
-        track = this.setPathCorrectOrder(track, source, destination);
-        float length = this.getPathLength(track);
-  
-        Route route = new Route(track,length);
-        route.destinationPlatform = null;
-        route.destinationParkingSpot = ps;
-        return route;
-      
-    }
     
      /*
      * Calculates distance from current platform of agv to desination platform.
@@ -192,25 +130,7 @@ public class Road implements Serializable
      * The shortest way is converted into a Route.
      */
     
-    public Route calculateShortestPath(Vector3f vehicleposition, Vector3f destination){ //only for mainroad
-        List<Vector3f> outsidetrack = new ArrayList<Vector3f>(this.track);
-
-        outsidetrack.add(destination);
-        Vector3f positionvehicle = vehicleposition;
-        outsidetrack.add(positionvehicle);
-        Collections.sort(outsidetrack);
-        List<Vector3f> insidetrack = new ArrayList<>(outsidetrack);
-        Collections.reverse(outsidetrack);
-        
-        insidetrack = this.setPathCorrectOrder(insidetrack, positionvehicle, destination);
-        outsidetrack = this.setPathCorrectOrder(outsidetrack,positionvehicle, destination);
-        
-        float length_insidetrack = getPathLength(insidetrack);
-        float length_outsidetrack = getPathLength(outsidetrack);
-        
-        if (length_insidetrack < length_outsidetrack){return new Route(insidetrack, length_insidetrack);}
-        else return new Route(outsidetrack, length_outsidetrack);
-    }
+ /*
     public Route calculateShortestPath(Vehicle vehicle, Vector3f destination){ //only for mainroad
         List<Vector3f> outsidetrack = new ArrayList<Vector3f>(this.track);
 
@@ -229,31 +149,74 @@ public class Road implements Serializable
         
         if (length_insidetrack < length_outsidetrack){return new Route(insidetrack, length_insidetrack);}
         else return new Route(outsidetrack, length_outsidetrack);
-    }
+    }*/
     
-    public List<Vector3f> setPathCorrectOrder(List<Vector3f> path, Vector3f source, Vector3f destination){
-
-        System.out.println("Source: " + source);
-        System.out.println("Destination: " + destination);
-        int indexSource = path.indexOf(source);
-        int indexDestination =  path.indexOf(destination);
-        System.out.println("indexSource: " + indexSource);
-        System.out.println("pathSize: " + path.size());
-     
+    public List<Vector3f> setPathCorrectOrder(List<Vector3f> path, Platform source, Platform destination){
+        //path size altijd 4
+        List<Vector3f> correctPath = path;
+        correctPath.add(path.get(0));
+        correctPath.add(path.get(1));
         
-        List<Vector3f> weg1 = new ArrayList(path.subList(indexSource, path.size()));
-    
-        
-       
-        List<Vector3f> weg2 = new ArrayList(path.subList(0, indexSource));
-        List<Vector3f> weg = new ArrayList<Vector3f>(weg1);
-        weg.addAll(weg2);
-        try{
-        weg = new ArrayList(weg.subList(0, indexDestination+1));}
-        catch(Exception e){
-            //destination is laatste waypoint
+        boolean right = true;
+        //bepalen aan hand van platform of rechts of links: nu altijd rechtsom
+        if (source instanceof StoragePlatform)
+        {
+            right = false;
         }
-        return weg;
+        //if (typeOrInterface.isInstance(someObject);)
+        
+        //rechtsom
+        if (true)
+        {
+            for (Vector3f waypoint : track)   
+            {
+              if ((waypoint.x >= source.getExitpoint().x && waypoint.z >= source.getExitpoint().z))
+              {
+                  
+                correctPath.add(waypoint);
+              }  
+             
+            }
+            
+            for(Vector3f waypoint : track)
+            {
+              if ((waypoint.x >= destination.getEntrypoint().x && waypoint.z >= destination.getEntrypoint().z))
+              {
+                 correctPath.add(waypoint);
+              }
+            }
+            
+    
+        }
+        
+        //linksom <= 
+        
+        else
+        {
+            for (Vector3f waypoint : track)   
+            {
+              if ((waypoint.x <= source.getExitpoint().x && waypoint.z <= source.getExitpoint().z))
+              {
+                  
+                correctPath.add(waypoint);
+              }  
+             
+            }
+            
+            for(Vector3f waypoint : track)
+            {
+              if ((waypoint.x <= destination.getEntrypoint().x && waypoint.z <= destination.getEntrypoint().z))
+              {
+                 correctPath.add(waypoint);
+              }
+            }
+            
+        }
+        
+        correctPath.add(path.get(2));
+        correctPath.add(path.get(3));
+
+        return correctPath;
     }
     
     

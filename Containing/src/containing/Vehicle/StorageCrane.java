73f5003f22e37a8227376bef05f3961d313b030f
Gemirt <mairimoz@hotmail.com>
2014-01-09 11:53:01 +0100

    unloadStorageCrane

--- Containing/src/containing/Vehicle/StorageCrane.java
+++ Containing/src/containing/Vehicle/StorageCrane.java
@@ -29,7 +29,8 @@ public class StorageCrane extends Crane {
     public static float width = 5f; //????????
     public static float length = 6f; //??????????
     //hier StorageCrane specific variables
-   
+    private Vector3f containerStoragePosition = null;
+    private Vector3f defaultPositionStorageStrip = null;
     public StorageCrane(Vector3f startPosition, Platform platform){ //variabelen doorgeven aan constructor crane
         super(startPosition, platform, Type.STORAGECRANE, width, length);
 
@@ -39,16 +40,19 @@ public class StorageCrane extends Crane {
     public void load(AGV agv, Vector3f containerStoragePosition, int i, Vector3f defaultPositionStorageStrip) throws VehicleOverflowException, CargoOutOfBoundsException, Exception{ //container from extern verhicle
     try
         {
+            this.containerStoragePosition = containerStoragePosition;
+            this.defaultPositionStorageStrip =  defaultPositionStorageStrip;
+            
             List<Vector3f> path = new ArrayList<Vector3f>();
             
             super.load(agv.unload());
             this.getCargo().get(0).setArrivalPosition(agv.getPosition());
             
-            this.loadTime = 20*10;  
-            this.unloadTime = 12*10;
+            this.loadTime = 14*10;  
+            this.unloadTime = 14*10;
             path.add(this.position);
             path.add(defaultPositionStorageStrip);
-            
+          
             /*
             path.add(agv.getPosition());
             path.add(this.position);
@@ -78,6 +82,26 @@ public class StorageCrane extends Crane {
         //this.timeCountDown = (int) liftTimeMax;
         //update: while (this.timeCounter < starttime + liftTimeMax + moveContainerSpeed * 2){} //aan het laden
         //roep evt nieuwe agv aan (op zelfde parkeerplaats of op parkeerplaats opzij [moet platform doen]
+    }
+    
+    //from agvspot to containerStoragePosition
+    public void unload(Vector3f containerStoragePosition)
+    {
+        List<Vector3f> path = new ArrayList<Vector3f>();
+        path.add(this.position); 
+        path.add(this.containerStoragePosition);
+        
+        HashMap<String, Object> map = new HashMap<>();
+            
+             map.put("craneid", this.getID());
+             map.put("vehicleType", this.getVehicleType());
+          
+             map.put("duration", this.unloadTime);
+            
+             map.put("path", path);
+
+             CommandHandler.addCommand(new Command("unloadStorageCrane", map));
+        
     }
     
      @Override

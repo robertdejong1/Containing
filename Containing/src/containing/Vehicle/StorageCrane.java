fb8c292ccf446d8d736ff588a0bb95d0b2ed8d19
Gemirt <mairimoz@hotmail.com>
2014-01-09 14:22:07 +0100

    loadtime

--- Containing/src/containing/Vehicle/StorageCrane.java
+++ Containing/src/containing/Vehicle/StorageCrane.java
@@ -54,7 +54,7 @@ public class StorageCrane extends Crane {
             
             this.getCargo().get(0).setArrivalPosition(agv.getPosition());
             
-            this.loadtime = 14;  
+            this.loadtime = 14 * 10;  
             this.unloadtime = 14;
             path.add(this.position);
             path.add(defaultPositionStorageStrip);

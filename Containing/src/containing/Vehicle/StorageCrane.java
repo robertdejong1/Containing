54aa13c2f94b665cb17fee404d6f53eba3782872
Gemirt <mairimoz@hotmail.com>
2014-01-10 13:20:48 +0100

    25sec

--- Containing/src/containing/Vehicle/StorageCrane.java
+++ Containing/src/containing/Vehicle/StorageCrane.java
@@ -55,7 +55,7 @@ public class StorageCrane extends Crane {
             this.getCargo().get(0).setArrivalPosition(agv.getPosition());
             
             this.loadtime = 14 * 10;  
-            this.unloadtime = 20 * 10;
+            this.unloadtime = 25 * 10;
             path.add(this.position);
             path.add(defaultPositionStorageStrip);
             this.status = Status.LOADING;
@@ -96,7 +96,7 @@ public class StorageCrane extends Crane {
     public void unload(Vector3f containerStoragePosition)
     {
         Container container = super.unload();
-        this.unloadtime = 20 * 10;
+        this.unloadtime = 25 * 10;
         List<Vector3f> path = new ArrayList<Vector3f>();
         path.add(this.position); 
         path.add(this.containerStoragePosition);

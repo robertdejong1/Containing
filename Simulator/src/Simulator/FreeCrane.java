9b5c6a53f568ab664463636a7790ab179a99365b
benjamin <benjaminfeenstra@hotmail.com>
2014-01-13 14:36:35 +0100

    jmok

--- Simulator/src/Simulator/FreeCrane.java
+++ Simulator/src/Simulator/FreeCrane.java
@@ -5,6 +5,7 @@
 package Simulator;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.math.FastMath;
 import com.jme3.scene.Node;
 
 
@@ -16,6 +17,7 @@ public class FreeCrane extends Model
         super(assetManager, "Models/freecrane.j3o", node);
         super.model.setName("FreeCrane " + id);
         super.model.scale(0.5f);
+        super.model.rotate(0, 90*FastMath.DEG_TO_RAD, 0);
     }
     
 }

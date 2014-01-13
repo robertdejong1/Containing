3c2cd18d69f144fc7c521b8476537a2bb235a13a
robertdj20 <robertdj20@gmail.com>
2014-01-13 11:45:55 +0100

    Capciteit fix

--- Containing/src/containing/Vehicle/Barge.java
+++ Containing/src/containing/Vehicle/Barge.java
@@ -20,9 +20,9 @@ public class Barge extends ExternVehicle {
 
     public static float width = 5f; //????????
     public static float length = 10f; //??????????
-    public static int nrContainersDepth = 6;
-    public static int nrContainersHeight = 6;
-    public static int nrContainersWidth = 10;
+    public static int nrContainersDepth = 20;
+    public static int nrContainersHeight = 20;
+    public static int nrContainersWidth = 20;
 
     
     public Barge(Date arrivalDate, float arrivalTime, Platform platform, String company)
